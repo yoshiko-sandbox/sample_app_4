c1f0f0dbf9af1ee2a280ff254aa0bdaad7604d56
maasa <maasa.giss2@gmail.com>
2014-05-13 19:00:00 +0900

    セキュリティのアップデート

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,7 +19,7 @@ class User < ActiveRecord::Base
   has_many :followers, through: :reverse_relationships, source: :follower
 
   before_save { email.downcase! }
-  before_save :create_remember_token
+  before_create :create_remember_token
 
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -44,8 +44,16 @@ class User < ActiveRecord::Base
     relationships.find_by_followed_id(other_user.id).destroy
   end
 
+  def User.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def User.encrypt(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
   private
     def create_remember_token
-      self.remember_token = SecureRandom.urlsafe_base64
+      self.remember_token = User.encrypt(User.new_remember_token)
     end
 end
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -86,7 +86,7 @@ describe "Authentication" do
       describe "when attempting to visit a protected page" do
         before do
           visit edit_user_path(user)
-          sign_in user
+          sign_in user, no_capybara: true
         end
 
         describe "after signing in" do
@@ -147,15 +147,15 @@ describe "Authentication" do
     describe "as wrong user" do
       let(:user) { FactoryGirl.create(:user) }
       let(:wrong_user) { FactoryGirl.create(:user, email: "wrong@example.com") }
-      before { sign_in user }
+      before { sign_in user, no_capybara: true }
 
       describe "visiting Users#edit page" do
         before { visit edit_user_path(wrong_user) }
         it { should_not have_title(full_title('Edit user')) }
       end
 
-      describe "submitting a PUT request to the Users#update action" do
-        before { put user_path(wrong_user) }
+      describe "submitting a PATCH request to the Users#update action" do
+        before { patch user_path(wrong_user) }
         specify { expect(response).to redirect_to(root_path) }
       end
 
@@ -165,7 +165,7 @@ describe "Authentication" do
       let(:user) { FactoryGirl.create(:user) }
       let(:non_admin) { FactoryGirl.create(:user) }
 
-      before { sign_in non_admin }
+      before { sign_in non_admin, no_capybara: true }
 
       describe "submitting a DELETE request to the Users#destroy action" do
         before { delete user_path(user) }
